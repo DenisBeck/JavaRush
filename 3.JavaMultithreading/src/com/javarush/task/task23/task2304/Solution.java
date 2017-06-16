5cc892f35cec027014260496962aaf36744a570b
DenisBeck <beckass86@gmail.com>
2017-06-16 23:25:42 +0500

    level 23-24 complete

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2304/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task23.task2304;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/*
+Inner 3
+*/
+public class Solution {
+
+    private List<Task> tasks;
+    private List<String> names;
+
+    private DbDataProvider taskDataProvider = new TaskDataProvider();
+    private DbDataProvider nameDataProvider = new NameDataProvider();
+
+    public void refresh() {
+        Map taskCriteria = ViewMock.getFakeTasksCriteria();
+        taskDataProvider.refreshAllData(taskCriteria);
+
+        Map nameCriteria = ViewMock.getFakeNamesCriteria();
+        nameDataProvider.refreshAllData(nameCriteria);
+    }
+
+    private interface DbDataProvider<T> {
+        void refreshAllData(Map criteria);
+    }
+
+    class Task {
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private class TaskDataProvider implements DbDataProvider<Solution.Task> {
+
+        @Override
+        public void refreshAllData(Map criteria) {
+            List<Solution.Task> list = DbMock.getFakeTasks(criteria);
+            tasks = new ArrayList<>(list);
+        }
+    }
+
+    private class NameDataProvider implements DbDataProvider<String> {
+
+        @Override
+        public void refreshAllData(Map criteria) {
+            List<String> list = DbMock.getFakeNames(criteria);
+            names = new ArrayList<>(list);
+
+        }
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by Dennis on 16.06.2017.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public void move() {}
+
+    public void fly() {}
+}
